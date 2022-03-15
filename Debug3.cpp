db10d338359ed4f240ec2dfc64b38d2c329a08f6
Nolan Pawelski <32725534+pawnol@users.noreply.github.com>
2022-03-14 21:07:08 -0500

    Add files via upload

---
+++ Debug3.cpp
@@ -0,0 +1,22 @@
+/*
+Debug 3
+Pawelski
+3/14/2022
+Please follow the instructions on the activity sheet for what needs
+to be completed.
+*/
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string number1, number2, sum;
+    std::cout << "Enter a number >> ";
+    std::cin >> number1;
+    std::cout << "Enter another number >> ";
+    std::cin >> number2;
+    sum = number1 + number2;
+    std::cout << sum;
+    return 0;
+}
